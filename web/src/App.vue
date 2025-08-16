5f09d76d5c1aa010b1f01b4bab1b14a8b69f88e2
zhezhen fang <202546355@qq.com>
2025-08-16 19:24:18 +0800

    jjjjj0

---
+++ web/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>Bot昵称: {{bot_name}}</div>
+    <div>Bot战力: {{bot_rating}}</div>
+  </div>
+  <router-view/>
+</template>
+
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+export default{
+  name: "App",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    $.ajax({
+      url:"http://127.0.0.1:3000/pk/getBotInfo/",
+      type: "get",
+      success: resp => {
+        console.log(resp);
+      }
+    });
+
+    return{
+      bot_name,
+      bot_rating
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
